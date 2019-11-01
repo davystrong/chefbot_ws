afe694fb137d12dd347db09d57ab52aee6ef47b6
David Armstrong <david.armstrong.br@gmail.com>
2019-11-01 10:11:36 -0300

    Added devel directory

--- src/wheels_pid/scripts/talker.py
+++ src/wheels_pid/scripts/talker.py
@@ -2,15 +2,16 @@
 
 #test
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32
 from geometry_msgs.msg import Twist
 
 def talker():
     pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=5)
+    # pub = rospy.Publisher('left_wheel_speed', Float32, queue_size=5)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(30) # 30hz
     while not rospy.is_shutdown():
-
+        # pub.publish(10.0)
         twist = Twist()
         twist.linear.x = 1; twist.linear.y = 0; twist.linear.z = 0
         twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 1
