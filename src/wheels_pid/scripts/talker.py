2a61957f3b1f415beaba1e9c408deadcbed8c04a
David Armstrong <david.armstrong.br@gmail.com>
2019-10-31 23:51:07 -0300

    Talker.py agora faz o robo girar

--- src/wheels_pid/scripts/talker.py
+++ src/wheels_pid/scripts/talker.py
@@ -3,15 +3,18 @@
 #test
 import rospy
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=5)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+
+        twist = Twist()
+        twist.linear.x = 1; twist.linear.y = 0; twist.linear.z = 0
+        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 1
+        pub.publish(twist)
         rate.sleep()
 
 if __name__ == '__main__':
