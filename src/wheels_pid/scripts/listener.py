ee961cd35559c7454dba9e1fb1e6a612074c6112
Monaretto <57225228+Monaretto@users.noreply.github.com>
2019-11-02 22:18:49 -0300

    Add files via upload
    Subscriber of wheels speed implemented

--- src/wheels_pid/scripts/listener.py
+++ src/wheels_pid/scripts/listener.py
@@ -2,23 +2,30 @@
 
 import rospy
 from std_msgs.msg import String
+import sensor_msgs
+from sensor_msgs.msg import JointState
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
 
-def listener():
+def actuatorCallback(jointState):
+    # here you run your PID control, each time the subscriber collects data
+    rospy.loginfo('\r\nleft speed is: %f, \r\nrigth speed is %f',
+                  jointState.velocity[0],
+                  jointState.velocity[1])
+
+def listener(name):
 
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber('chatter', String, callback)
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber(name, JointState, actuatorCallback)
 
     # spin() simply keeps python from exiting until this node is stopped
+
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    listener('joint_states')
