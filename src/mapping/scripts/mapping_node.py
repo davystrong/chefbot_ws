35440a837104f692789181a08ab2fa85d074916a
Monaretto <57225228+Monaretto@users.noreply.github.com>
2019-11-12 01:11:49 -0300

    implementado mapa, com matrix de viz e mapa real
    Para rodar o código, basta compilar o sistema, e rodar o nodo do mapping_node.py. Para visualizar as matrizes, basta abrir o rviz e visualiza-las lá. 
    TODO: rotacionar o robo para fazer varredura 360 graus.
    TODO: verificar o que fazer com informação Nan
    TODO: implementar incremento do angulo geral do robo e em sua translação, para assim mudar o sistemas de cordenadas do robo.

--- src/mapping/scripts/mapping_node.py
+++ src/mapping/scripts/mapping_node.py
@@ -4,17 +4,20 @@
 import rospy
 import cv2
 import numpy as np
+import math
 from sensor_msgs.msg import LaserScan, Image
 from cv_bridge import CvBridge
 
 #0(preto) = sabemos que está ocupado (1)
 #255(branco) = sabemos que está livre (0)
 #100(cinza) = não sabemos (-1)
-img = np.zeros((200,200))
+img = np.ones((200,200), np.float32) * 100
 hasImage = False
 LaserScanGlobal = {}
+angle_runned = 0
 
-pub = rospy.Publisher('/traj_output', Image, queue_size=10)
+pub = rospy.Publisher('/traj_output_teste', Image, queue_size=10)
+pub2 = rospy.Publisher('/vision_now_matrix', Image, queue_size=10)
 
 def Publish_Image():
     global img
@@ -22,76 +25,64 @@ def Publish_Image():
     msg = br.cv2_to_imgmsg(img)
     pub.publish(msg)
 
+def Publish_Image_Now(img_now):
+    br = CvBridge()
+    msg = br.cv2_to_imgmsg(img_now)
+    pub2.publish(msg)
+
 def CalculateNewRotationMatrix(angleRunned):
     return [[np.cos(angleRunned), np.sin(angleRunned)],[-np.sin(angleRunned),np.cos(angleRunned)]]
 
-def kinectCallback(LaserScan):
+def kinectCallback(LaserScanLocal):
     global LaserScanGlobal
     global hasImage
-    LaserScanGlobal = LaserScan
+    global angle_runned
+    angle = 0
+    x_a = 100
+    y_a = 100
+    LaserScanGlobal = LaserScanLocal
     hasImage = True
-    cv2.imshow('mapa', img)
+    img_now = np.ones((200,200), np.float32) * 100
+    rotationMatrix = CalculateNewRotationMatrix(angle_runned)
+    angle = LaserScanLocal.angle_min
+    for i in LaserScanLocal.ranges:
+        angle = LaserScanLocal.angle_increment + angle
+        if not math.isnan(i):
+            #neste caso foi encontrado um obejto
+            #calcula a distancia, nos eixos x e y, entre o objeto encontrado e o centro de massa do robo
+            y_dist = i * np.sin(angle)
+            x_dist = i * np.cos(angle)
+           # print(angle)
+            #rotaciona a distancia a partir da posição atual do robo
+            x_dist_rotated = x_dist * rotationMatrix[0][0] + y_dist * rotationMatrix[0][1]
+            y_dist_rotated = x_dist * rotationMatrix[1][0] + y_dist * rotationMatrix[1][1]
+            #transforma a distancia calculada em pixels
+            x_pix = int(x_dist_rotated * 10 + x_a)
+            y_pix = int(y_dist_rotated * 10 + y_a)
+            #traça uma linha branca indicando que tudo até o objeto encontrado não está ocupado
+            cv2.line(img,(x_a,y_a),(x_pix , y_pix),255,1)
+            cv2.line(img_now,(x_a,y_a),(x_pix , y_pix),255,1)
+
+            cv2.line(img,(x_pix , y_pix),(x_pix , y_pix),0,1)
+            cv2.line(img_now,(x_pix , y_pix),(x_pix , y_pix),0,1)
+            #pinta o píxel encontrado de preto indicando que o mesmo está ocupado
+    x_dist_rotated = 5 * rotationMatrix[0][0]
+    y_dist_rotated = 5 * rotationMatrix[1][0]
+    x_pix = int(x_dist_rotated * 10 + x_a)
+    y_pix = int(y_dist_rotated * 10 + y_a)
+    cv2.line(img_now,(x_a,y_a),(x_pix , y_pix), 180, 1)
+    Publish_Image_Now(img_now)
+
 
 def map():
     global maxAngle
     global LaserScanGlobal
     global hasImage
     global img
-    x_a = 100
-    y_a = 100
-    rotationMatrix = CalculateNewRotationMatrix(map.angleRunned)
     while not rospy.core.is_shutdown():
-        if hasImage:
-            for i in LaserScanGlobal.ranges:
-                try:
-                    #neste caso foi encontrado um obejto
-                    #calcula a distancia, nos eixos x e y, entre o objeto encontrado e o centro de massa do robo
-                    map.angle = LaserScanGlobal.angle_increment + map.angle
-                    y_dist = i * np.sin(map.angle)
-                    x_dist = i * np.cos(map.angle)
-
-                    #rotaciona a distancia a partir da posição atual do robo
-                    x_dist_rotated = x_dist * rotationMatrix[0][0] + y_dist * rotationMatrix[0][1]
-                    y_dist_rotated = x_dist * rotationMatrix[1][0] + y_dist * rotationMatrix[1][1]
-
-                    #transforma a distancia calculada em pixels
-                    x_pix = x_dist_rotated * 0.5 + x_a
-                    y_pix = y_dist_rotated * 0.5 + y_a
-                    #traça uma linha branca indicando que tudo até o objeto encontrado não está ocupado
-                    cv2.line(img,(x_a,y_a),(x_pix , y_pix),255,1)
-                    #pinta o píxel encontrado de preto indicando que o mesmo está ocupado
-                    img[x_pix, y_pix] = 0
-                    map.angle = LaserScan.angle_increment + map.angle
-                    Publish_Image()
-
-                except:
-                    #neste caso não foi encontrado nenhum objeto no campo de visão da câmera
-                    #calcula a distancia, nos eixos x e y, entre o objeto encontrado e o centro de massa do robo
-                    map.angle = LaserScanGlobal.angle_increment + map.angle
-                    y_dist = LaserScanGlobal.range_max * np.sin(map.angle)
-                    x_dist = LaserScanGlobal.range_max * np.cos(map.angle)
-
-                    #rotaciona a distancia a partir da posição atual do robo
-                    x_dist_rotated = x_dist * rotationMatrix[0][0] + y_dist * rotationMatrix[0][1]
-                    y_dist_rotated = x_dist * rotationMatrix[1][0] + y_dist * rotationMatrix[1][1]
-
-                    #transforma a distancia calculada em pixels
-                    x_pix = int(x_dist_rotated * 0.5 + x_a)
-                    y_pix = int(y_dist_rotated * 0.5 + y_a)
-                    #traça uma linha branca indicando que tudo até o objeto encontrado não está ocupado
-                    cv2.line(img,(x_a,y_a),(x_pix , y_pix),255,1)
-                    Publish_Image()
+        Publish_Image()
 
 
-
-
-            #rotate 90 degrees
-            map.angleRunned += 90
-            rotationMatrix = CalculateNewRotationMatrix(map.angleRunned)
-            hasImage = False
-
-map.angle = 0
-map.angleRunned = 0
 
 if __name__ == '__main__':
     rospy.init_node('mapping_node', anonymous=True)
