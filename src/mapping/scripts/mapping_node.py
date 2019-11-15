069a0fe5448bd6eacf6519cd68bef69099cc8761
Monaretto <57225228+Monaretto@users.noreply.github.com>
2019-11-15 16:05:01 -0300

    Add files via upload

--- src/mapping/scripts/mapping_node.py
+++ src/mapping/scripts/mapping_node.py
@@ -5,8 +5,10 @@ import rospy
 import cv2
 import numpy as np
 import math
+from tf.transformations import euler_from_quaternion
 from sensor_msgs.msg import LaserScan, Image
 from cv_bridge import CvBridge
+from nav_msgs.msg import Odometry
 
 #0(preto) = sabemos que está ocupado (1)
 #255(branco) = sabemos que está livre (0)
@@ -15,19 +17,22 @@ img = np.ones((200,200), np.float32) * 100
 map_matrix = np.ones((200,200), np.float32) * (-1)
 hasImage = False
 LaserScanGlobal = {}
-angle_runned = 0
+angle_Yaw = 0
+x_a_moved = 0
+y_a_moved = 0
+
 
 pub = rospy.Publisher('/traj_output_teste', Image, queue_size=10)
 pub2 = rospy.Publisher('/vision_now_matrix', Image, queue_size=10)
 
-def TranslateImageToMap(image)
-    mp = image
+def TranslateImageToMap(img):
+    mp = img
     mp[mp==0] = 1
     mp[mp==255] = 0
     mp[mp>1] = -1
     return mp
 
-def TranslateMapToImage(mp)
+def TranslateMapToImage(mp):
     image = mp
     image[image==1] = 0
     image[image==0] = 255
@@ -48,17 +53,29 @@ def Publish_Image_Now(img_now):
 def CalculateNewRotationMatrix(angleRunned):
     return [[np.cos(angleRunned), np.sin(angleRunned)],[-np.sin(angleRunned),np.cos(angleRunned)]]
 
+def movementCallback(Odometer):
+    global x_a_moved
+    global y_a_moved
+    global angle_Yaw
+    x_a_moved = Odometer.pose.pose.position.x
+    y_a_moved = Odometer.pose.pose.position.y
+    (roll,pitch,yaw) = euler_from_quaternion(Odometer.pose.pose.orientation)
+    angle_Yaw = yaw
+
 def kinectCallback(LaserScanLocal):
     global LaserScanGlobal
     global hasImage
-    global angle_runned
+    global angle_Yaw
     angle = 0
-    x_a = 100
-    y_a = 100
+    global x_a_moved
+    global y_a_moved
+    x_a = 100 + x_a_moved
+    y_a = 100 + y_a_moved
     LaserScanGlobal = LaserScanLocal
     hasImage = True
     img_now = np.ones((200,200), np.float32) * 100
-    rotationMatrix = CalculateNewRotationMatrix(angle_runned)
+    rotationMatrix = CalculateNewRotationMatrix(angle_Yaw)
+    print(angle_Yaw)
     angle = LaserScanLocal.angle_min
     for i in LaserScanLocal.ranges:
         angle = LaserScanLocal.angle_increment + angle
@@ -80,37 +97,16 @@ def kinectCallback(LaserScanLocal):
 
             cv2.line(img,(x_pix , y_pix),(x_pix , y_pix),0,1)
             cv2.line(img_now,(x_pix , y_pix),(x_pix , y_pix),0,1)
-            #pinta o píxel encontrado de preto indicando que o mesmo está ocupado
-        else:
-            #neste caso não foi encontrado um obejto
-            #calcula a distancia, nos eixos x e y, entre o centro de massa do robo e o alcance máximo do sensor
-            y_dist = LaserScanLocal.range_max * np.sin(angle)
-            x_dist = LaserScanLocal.range_max * np.cos(angle)
-			
-            #rotaciona a distancia a partir da posição atual do robo
-            x_dist_rotated = x_dist * rotationMatrix[0][0] + y_dist * rotationMatrix[0][1]
-            y_dist_rotated = x_dist * rotationMatrix[1][0] + y_dist * rotationMatrix[1][1]
-            #transforma a distancia calculada em pixels
-            x_pix = int(x_dist_rotated * 10 + x_a)
-            y_pix = int(y_dist_rotated * 10 + y_a)
-            #traça uma linha branca indicando que tudo até o objeto encontrado não está ocupado
-            cv2.line(img,(x_a,y_a),(x_pix , y_pix),255,1)
-            cv2.line(img_now,(x_a,y_a),(x_pix , y_pix),255,1)
-            
+            #pinta o píxel encontrado de preto indicando que o mesmo está ocupado       
     x_dist_rotated = 5 * rotationMatrix[0][0]
     y_dist_rotated = 5 * rotationMatrix[1][0]
     x_pix = int(x_dist_rotated * 10 + x_a)
     y_pix = int(y_dist_rotated * 10 + y_a)
     cv2.line(img_now,(x_a,y_a),(x_pix , y_pix), 180, 1)
-    map_matrix = TranslateImageToMap(img)
     Publish_Image_Now(img_now)
 
 
 def map():
-    global maxAngle
-    global LaserScanGlobal
-    global hasImage
-    global img
     while not rospy.core.is_shutdown():
         Publish_Image()
 
@@ -119,6 +115,7 @@ def map():
 if __name__ == '__main__':
     rospy.init_node('mapping_node', anonymous=True)
     rospy.Subscriber('/scan', LaserScan, kinectCallback)
+    rospy.Subscriber('/odom', LaserScan, kinectCallback)
     map()
 
     pass
